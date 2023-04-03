29a4ab0ff848d3bf7d2ed8e094b0ed96345cb96a
unknown <niluferokay@gmail.com>
2023-04-03 19:33:00 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,9 @@ function App() {
             <li className="nav__item">
               <a href="/NiluferOkay_CV.pdf" target="_blank" className="nav__link">CV</a>
             </li>
+            <li className="nav__item">
+              <a href="mailto:niluferokay@gmail.com" className="nav__link">Contact</a>
+            </li>
             <li className="nav__item__icon">
               <a href="https://www.linkedin.com/in/niluferokay/" target="_blank" className="nav__link__icon">
                 <i class="ri-linkedin-box-fill"></i></a>
@@ -31,8 +34,8 @@ function App() {
                 <i class="ri-github-fill"></i></a>
             </li>
             <li className="nav__item__icon">
-              <a href="mailto:niluferokay@gmail.com" className="nav__link__icon">
-              <i class="ri-mail-fill"></i></a>
+            <a href="https://twitter.com/niluokay" target="_blank" className="nav__link__icon">
+            <i class="ri-twitter-fill"></i></a>            
             </li>
           </ul>
         </div>
@@ -47,13 +50,17 @@ function App() {
               <a href="/NiluferOkay_CV.pdf" target="_blank" className="nav__link"
               onClick={() => setActive(!active)}>CV</a>
             </li>
+            <li className="nav__item">
+              <a href="mailto:niluferokay@gmail.com" className="nav__link"
+              onClick={() => setActive(!active)}>Contact</a>
+            </li>
             <li className="nav__item__icon">
               <a href="https://www.linkedin.com/in/niluferokay/" target="_blank" className="nav__link__icon">
                 <i class="ri-linkedin-box-fill"></i></a>
                 <a href="https://github.com/niluferokay" target="_blank" className="nav__link__icon">
                 <i class="ri-github-fill"></i></a>
-                <a href="mailto:niluferokay@gmail.com" className="nav__link__icon">
-              <i class="ri-mail-fill"></i></a>
+                <a href="https://twitter.com/niluokay" target="_blank" className="nav__link__icon">
+                <i class="ri-twitter-fill"></i></a>
             </li>
           </ul>
           
@@ -73,11 +80,11 @@ function App() {
           <img src="profile.jpg" alt="" className="home__img" />
         </div>
         <div className="home__description">
-            <p className='home__para'> Hello, I'm Nilufer!</p> 
-            <p className='home__para'>I recently received my Master's degree in <a className='para__link' href="http://bis.boun.edu.tr/"  target="_blank">Business Information Systems</a> from Boğaziçi University. In my thesis, I developed a blockchain application to assess the environmental and social sustainability performance of the supply chain.</p>
-            <p className='home__para'>As a full-stack blockchain developer, I am very passionate about sustainability and interested in pursuing a career in regenerative finance (ReFi). I have always been fascinated by the potential of technology to solve complex problems, particularly to address social and environmental challenges. I see blockchain as a powerful tool for creating transparent, secure, and decentralized systems that can help mitigate the impacts of climate change and promote regenerative practices.</p>
-            <p className='home__para'>My biggest dream is to use my skills and expertise to help create innovative solutions that can make a positive impact on the environment and the world at large. I am currently exploring job opportunities in ReFi companies and DAOs. I have experience working with smart contracts and decentralized applications. I am also experienced in multiple programming languages and web frameworks, including React, Node.js, and Solidity. I am passionate about working in a creative and collaborative work environment.</p>
-            <p className='home__para'>Apart from coding, I enjoy <a className='para__link' href="https://www.goodreads.com/niluokay" target="_blank">reading</a>, writing, doing yoga and walking in nature.</p>
+            <p className='home__para'>Hi there, my name is Nilufer, and I'm excited to share my passion for blockchain technology and regenerative finance (ReFi) with you!</p> 
+            <p className='home__para'>I recently earned my master's degree in <a className='para__link' href="http://bis.boun.edu.tr/"  target="_blank">Business Information Systems</a> from Boğaziçi University, during which I developed a blockchain-based decision support system to assess the environmental and social sustainability performance of the supply chain and track and trace the product journey.</p> 
+            <p className='home__para'>This project opened my eyes to the immense potential of blockchain technology in addressing complex sustainability challenges by promoting transparency, security, and decentralization. As a full-stack blockchain developer with a background in finance, I'm eager to contribute my skills and expertise to ReFi initiatives that create a positive impact on the environment and the world.</p>
+            <p className='home__para'>I have experience in working with smart contracts and decentralized applications using various programming languages and web frameworks including Solidity, JavaScript, React, and Node.js. I'm now seeking opportunities to work in ReFi companies and decentralized autonomous organizations (DAOs) on innovative projects that promote regenerative practices and mitigate the impacts of climate change. I'm particularly excited about working in a creative and collaborative ecosystem where I can leverage my skills to drive meaningful change.</p>
+            <p className='home__para'>Apart from my work, I enjoy <a className='para__link' href="https://www.goodreads.com/niluokay" target="_blank">reading</a>, writing, practicing yoga, and exploring nature. Ultimately, my biggest dream is to help create a more sustainable and equitable world through technology.</p>
         </div>
       </div>
     </header>
